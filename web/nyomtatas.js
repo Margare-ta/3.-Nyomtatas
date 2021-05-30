17a75b92b3afc8ddf6a08801722cfce177076c4b
Margareta <margaretavida120@gmail.com>
2021-05-30 16:08:09 +0200

    first commit

---
+++ web/nyomtatas.js
@@ -0,0 +1,16 @@
+function kalkulal(){
+    //Űrlapadatok
+    const szelesseg=297;
+    const magassag=420;
+    const papir=document.getElementById('papirtipus').value;
+    //Számítások
+    let terulet=Math.round((szelesseg*magassag)/10000);    
+    let koltseg=terulet*papir;
+    //Megjelenítés
+    document.getElementById('valasz').style.visibility = "visible";
+}
+
+let kalkul = document.querySelector("#kalkulalGomb");
+
+kalkul.addEventListener("click", kalkulal);
+
